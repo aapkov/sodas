00955946f4090cdc6b442b5722d7ecdec2475cf4
aapkov <apkovapkov@gmail.com>
2022-08-15 16:46:54 +0200

    add config examples

--- config/config_example.js
+++ config/config_example.js
@@ -3,7 +3,12 @@ const config = {
     isModFormDisabled: false,
     SITE_KEY: '',
     SECRET_KEY: '',
-    SECRET: ''
+    SECRET: '',
+    EMAIL_ADRESS:'',
+    EMAIL_PASSWORD: '',
+    client_id: '',
+    client_secret: '',
+    client_refresh_token: ''
    };
    
    module.exports = config;
